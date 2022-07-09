af6fff31746aa3568600f71dc0004cd7928512df
qui <quivv.dtp@gmail.com>
2022-07-09 19:06:44 +0700

    Qui update

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.css';
+import Home from './component/Home';
+import Navbar from './component/Navbar';
 
 function App() {
   return (
     <div className="App">
-      Hello
+      <Navbar/>
+      <Home/>
     </div>
   );
 }
---
+++ src/component/Home.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+// import Products from "./Products";
+
+const Home = () => {
+  return (
+    <div className="Hero">
+      <div className="card bg-dark text-white border-0">
+        <img src="/assets/bg.webp" className="card-img" alt="Background" height="800px"/>
+        <div className="card-img-overlay d-flex flex-column justify-content-center">
+          <div className="container">
+            <h5 className="card-title display-3 fw-bold mb-0">NEW SEASON ARRIVALS</h5>
+            <p className="card-text lead fs-2">
+              CHECK OUT ON THE TRENDS
+            </p>
+          </div>
+        </div>
+      </div>
+      {/* <Products/> */}
+    </div>
+  );
+};
+
+export default Home;
