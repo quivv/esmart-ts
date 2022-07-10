73ecea33674849bb56bda700346ff8d85459cdc6
qui <quivv.dtp@gmail.com>
2022-07-10 15:25:09 +0700

    Qui add finish load product

--- src/component/Loading.tsx
+++ src/component/Loading.tsx
@@ -7,6 +7,15 @@ const Loading:React.FC = () => {
       <div className="col-md-6">
         <Skeleton height={400} width={400}/>
       </div>
+      <div className="col-md-6" style={{lineHeight:2}}>
+        <Skeleton height={50} width={300}/>
+        <Skeleton height={75}/>
+        <Skeleton height={25} width={150}/>
+        <Skeleton height={50}/>
+        <Skeleton height={150}/>        
+        <Skeleton height={50} width={300}/>
+
+      </div>
     </>
   ) 
 }
