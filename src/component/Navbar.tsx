27ee9a4dc1f6a8b1ad4022e723ef1d3788443fe2
qui <quivv.dtp@gmail.com>
2022-07-12 06:25:59 +0700

    fix bug

--- src/component/Navbar.tsx
+++ src/component/Navbar.tsx
@@ -4,7 +4,6 @@ import { useSelector } from "react-redux";
 import { ProductType } from "../Type";
 
 const Navbar = () => {
-  const state: ProductType[] = useSelector<ProductType>(x => x.handleCart)
   
   return (
     <div>
